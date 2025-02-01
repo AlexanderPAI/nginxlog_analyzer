35604c3fe8a3a1f9cc298cb922ef4ad3f254bea2
AlexanderPAI <lawlikeart@yandex.ru>
2025-02-01 15:44:49 +0300

    class Settings for setting config, parse config at default config_file_path

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+import logging
+
+from config import Settings
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    settings = Settings()
+    logger.info(settings.config)
+
+
 if __name__ == "__main__":
-    print("Hello")
+    main()
